5a9f333ea3ad5f2bacb0bcb0686b0812d81cdc82
Ahmet Emre Gürdal <gurdalahmetemre@disroot.org>
2026-01-03 20:35:22 +0300

    Beets hook to create LRC files as well

--- modules/user/apps/cli/music/beets.nix
+++ modules/user/apps/cli/music/beets.nix
@@ -2,6 +2,34 @@
 
 let
 	cfg = config.userSettings.beets;
+	exportLrc = pkgs.writeShellScript "beetsExportLrc" ''
+		set -euo pipefail
+		FILE="$1"
+
+		case "$FILE" in
+			*.m4a) ;;
+			*) exit 0 ;;
+		esac
+
+		LRC="''${FILE%.m4a}.lrc"
+
+		[ -f "$LRC" ] && exit 0
+
+		LYRICS="$(${lib.getExe pkgs.exiftool} -s3 -Lyrics "$FILE")" 
+		[ -z "$LYRICS" ] && exit 0
+
+		if ! printf '%s\n' "$LYRICS" | grep -Eq '^\[[0-9]{2}:[0-9]{2}\.[0-9]{2}\]'; then
+			exit 0
+		fi
+
+		printf '%s\n' "$LYRICS" > "$LRC"
+		echo "Created LRC: $LRC"
+	'';
+	beetConfig = pkgs.replaceVars ./beets.config.yaml {
+		musicDirectory = config.xdg.userDirs.music;
+		xdgDataDir = "${config.home.homeDirectory}/.local/share";
+		pathToScript = "${exportLrc}";
+	};
 in
 {
 	options = {
@@ -9,46 +37,9 @@ in
 	};
 
 	config = lib.mkIf cfg.enable {
-		programs.beets = {
-			enable = true;
-			settings = {
-				directory = config.xdg.userDirs.music;
-				library = "${config.home.homeDirectory}/.local/share/beets.dp";
-				import = {
-					move = true;
-				};
-				plugins = [
-					"musicbrainz"
-					"lyrics"
-					"fetchart"
-					"embedart"
-					"fromfilename"
-					"convert"
-					"replaygain"
-				];
-				lyrics = {
-					synced = true;
-				};
-				convert = {
-					auto = true;
-					delete_originals = true;
-					format = "AAC";
-					never_convert_lossy_files = true;
-					formats = {
-						aac = {
-							command = "ffmpeg -i $source -vn -c:a aac -q:a 6 $dest";
-							extension = "m4a";
-						};
-					};
-				};
-				replaygain = {
-					auto = true;
-					backend = "ffmpeg";
-					overwrite = true;
-					r128 = "flac m4a";
-					peak = "true";
-				};
-			};
-		};
+		home.packages = [
+			pkgs.beets
+		];
+		home.file.".config/beets/config.yaml".source = beetConfig;
 	};
 }
